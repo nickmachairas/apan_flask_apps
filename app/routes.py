ca586af3e5a50eed74286a29845a048decf2f375
Nick Machairas <nick@machairas.com>
2022-04-06 16:38:02 -0400

    basic search

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,14 @@ Author: Nick Machairas, 2022
 from flask import render_template
 from app import app
 from app.forms import SearchForm
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    port='5432',
+    database="uni_small",
+    user="postgres",
+    password="123")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,5 +22,10 @@ def home():
     form = SearchForm()
     search_results = None
     if form.validate_on_submit():
-        search_results = form.username.data
-    return render_template("home.html", form=form, search_results=search_results)
+        search_term = form.username.data
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM student WHERE name = '{search_term}';")
+        search_results = cur.fetchall()
+        cur.close()
+    return render_template(
+        "home.html", form=form, search_results=search_results)
